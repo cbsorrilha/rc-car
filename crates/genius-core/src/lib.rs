4fda14b041dc8732bfbeda3146ba3bc05ebbd9ab
Cesar de Barros <cesar@insiderstore.com.br>
2026-08-28 11:41:05 -0300

    fix: testes e state machine principal do genius

--- crates/genius-core/src/lib.rs
+++ crates/genius-core/src/lib.rs
@@ -20,12 +20,12 @@
  */
 #[derive(PartialEq)]
 #[derive(Debug)]
+#[derive(Clone)]
 pub enum Status {
     Startup,
     PlayingSequence,
     AwaitingInput,
     RoundSuccess,
-    RoundError,
     GameOver,
     GameCompleted
 }
@@ -33,9 +33,9 @@ pub enum Status {
 pub enum Event {
   GameStarted,
   ColorGiven,
-  // ColorPressed,
-  // SequencePlaybackCompleted,
-  // ResetRequested
+  ColorPressed,
+  ResetRequested,
+  SequencePlaybackCompleted,
 }
 
 #[derive(Debug)]
@@ -48,7 +48,36 @@ pub struct State {
 
 pub fn state_machine(state: &mut State, event: Event, color: u8) -> State {
   match event {
+    Event::ColorGiven => {
+      let i = state.current_sequence_size;
+      state.sequence[i] = color;
+      State {
+        status: state.status.clone(),
+        sequence: state.sequence,
+        current_sequence_size: i + 1,
+        next_player_try: state.next_player_try
+      }
+    }
+
     Event::GameStarted => {
+      State {
+        status: Status::PlayingSequence,
+        sequence: state.sequence,
+        current_sequence_size: state.current_sequence_size,
+        next_player_try: state.next_player_try,
+      }
+    }
+
+    Event::SequencePlaybackCompleted => {
+      State {
+        status: Status::AwaitingInput,
+        sequence: state.sequence,
+        current_sequence_size: state.current_sequence_size,
+        next_player_try: state.next_player_try
+      }
+    }
+
+    Event::ResetRequested => {
       State {
         status: Status::Startup,
         sequence: [0; 10],
@@ -56,14 +85,42 @@ pub fn state_machine(state: &mut State, event: Event, color: u8) -> State {
         next_player_try: 0,
       }
     }
-    Event::ColorGiven => {
-      let i = state.current_sequence_size;
-      state.sequence[i] = color;
+    
+    Event::ColorPressed => {
+      if state.status == Status::PlayingSequence {
+        return State {
+          status: state.status.clone(),
+          sequence: state.sequence,
+          current_sequence_size: state.current_sequence_size,
+          next_player_try: state.next_player_try,
+        }
+      }
+
+      if state.sequence[state.next_player_try] != color {
+        return State {
+          status: Status::GameOver,
+          sequence: state.sequence,
+          current_sequence_size: state.current_sequence_size,
+          next_player_try: state.next_player_try,
+        }
+      }
+
+      // o tamanho total do array de sequencia
+      if state.next_player_try == 9 {
+        return State {
+          status: Status::GameCompleted,
+          sequence: state.sequence,
+          current_sequence_size: state.current_sequence_size,
+          next_player_try: state.next_player_try,
+        }
+      }
+
+
       State {
-        status: Status::PlayingSequence,
+        status: Status::RoundSuccess,
         sequence: state.sequence,
-        current_sequence_size: i + 1,
-        next_player_try: state.next_player_try
+        current_sequence_size: state.current_sequence_size,
+        next_player_try: state.next_player_try + 1,
       }
     }
   }
@@ -73,22 +130,6 @@ pub fn state_machine(state: &mut State, event: Event, color: u8) -> State {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_start_event() {
-        let mut state = State {
-          status: Status::Startup,
-          sequence: [0; 10],
-          current_sequence_size: 0,
-          next_player_try: 0,
-        };
-
-        let resulting_state = state_machine(&mut state, Event::GameStarted, 0);
-        assert_eq!(resulting_state.status, state.status);
-        assert_eq!(resulting_state.sequence, state.sequence);
-        assert_eq!(resulting_state.current_sequence_size, state.current_sequence_size);
-        assert_eq!(resulting_state.next_player_try, state.next_player_try);
-    }
 
     #[test]
     fn test_color_given_event() {
@@ -103,16 +144,191 @@ mod tests {
         seq[0] = b'G';
 
         let desired_state = State {
-          status: Status::PlayingSequence,
+          status: Status::Startup,
           sequence: seq,
           current_sequence_size: 1,
           next_player_try: 0,
         };
 
         let resulting_state = state_machine(&mut initial_state, Event::ColorGiven, b'G');
-        assert_eq!(resulting_state.status, desired_state.status);
-        assert_eq!(resulting_state.sequence, desired_state.sequence);
-        assert_eq!(resulting_state.current_sequence_size, desired_state.current_sequence_size);
-        assert_eq!(resulting_state.next_player_try, desired_state.next_player_try);
+        assert_eq!(resulting_state.status, desired_state.status, "testing state.status resulting: {:#?} desired: {:#?}", resulting_state.status, desired_state.status);
+        assert_eq!(resulting_state.sequence, desired_state.sequence,"testing state.sequence resulting: {:?} desired: {:?}", resulting_state.sequence, desired_state.sequence);
+        assert_eq!(resulting_state.current_sequence_size, desired_state.current_sequence_size,"testing state.current_sequence_size resulting: {} desired: {}", resulting_state.current_sequence_size, desired_state.current_sequence_size);
+        assert_eq!(resulting_state.next_player_try, desired_state.next_player_try,"testing state.next_player_try resulting: {} desired: {}", resulting_state.next_player_try, desired_state.next_player_try);
+    }
+
+    #[test]
+    fn test_start_event() {
+        let mut seq = [0; 10];
+        seq[0] = b'G';
+
+        let mut initial_state = State {
+          status: Status::Startup,
+          sequence: seq,
+          current_sequence_size: 1,
+          next_player_try: 0,
+        };
+
+        let desired_state = State {
+          status: Status::PlayingSequence,
+          sequence: seq,
+          current_sequence_size: 1,
+          next_player_try: 0,
+        };
+
+        let resulting_state = state_machine(&mut initial_state, Event::GameStarted, 0);
+        assert_eq!(resulting_state.status, desired_state.status, "testing state.status resulting: {:#?} desired: {:#?}", resulting_state.status, desired_state.status);
+        assert_eq!(resulting_state.sequence, desired_state.sequence,"testing state.sequence resulting: {:?} desired: {:?}", resulting_state.sequence, desired_state.sequence);
+        assert_eq!(resulting_state.current_sequence_size, desired_state.current_sequence_size,"testing state.current_sequence_size resulting: {} desired: {}", resulting_state.current_sequence_size, desired_state.current_sequence_size);
+        assert_eq!(resulting_state.next_player_try, desired_state.next_player_try,"testing state.next_player_try resulting: {} desired: {}", resulting_state.next_player_try, desired_state.next_player_try);
+    }
+
+    #[test]
+    fn test_sequence_playback_completed() {
+        let mut seq = [0; 10];
+        seq[0] = b'G';
+
+        let mut initial_state = State {
+          status: Status::PlayingSequence,
+          sequence: seq,
+          current_sequence_size: 1,
+          next_player_try: 0,
+        };
+
+        let desired_state = State {
+          status: Status::AwaitingInput,
+          sequence: seq,
+          current_sequence_size: 1,
+          next_player_try: 0,
+        };
+
+        let resulting_state = state_machine(&mut initial_state, Event::SequencePlaybackCompleted, 0);
+        assert_eq!(resulting_state.status, desired_state.status, "testing state.status resulting: {:#?} desired: {:#?}", resulting_state.status, desired_state.status);
+        assert_eq!(resulting_state.sequence, desired_state.sequence,"testing state.sequence resulting: {:?} desired: {:?}", resulting_state.sequence, desired_state.sequence);
+        assert_eq!(resulting_state.current_sequence_size, desired_state.current_sequence_size,"testing state.current_sequence_size resulting: {} desired: {}", resulting_state.current_sequence_size, desired_state.current_sequence_size);
+        assert_eq!(resulting_state.next_player_try, desired_state.next_player_try,"testing state.next_player_try resulting: {} desired: {}", resulting_state.next_player_try, desired_state.next_player_try);
+    }
+
+    #[test]
+    fn test_color_pressed_event_round_fail() {
+        let mut seq = [0; 10];
+        seq[0] = b'G';
+
+        let mut initial_state = State {
+          status: Status::AwaitingInput,
+          sequence: seq,
+          current_sequence_size: 1,
+          next_player_try: 0,
+        };
+
+        let desired_state = State {
+          status: Status::GameOver,
+          sequence: seq,
+          current_sequence_size: 1,
+          next_player_try: 0,
+        };
+
+        let resulting_state = state_machine(&mut initial_state, Event::ColorPressed, b'R');
+        assert_eq!(resulting_state.status, desired_state.status, "testing state.status resulting: {:#?} desired: {:#?}", resulting_state.status, desired_state.status);
+        assert_eq!(resulting_state.sequence, desired_state.sequence,"testing state.sequence resulting: {:?} desired: {:?}", resulting_state.sequence, desired_state.sequence);
+        assert_eq!(resulting_state.current_sequence_size, desired_state.current_sequence_size,"testing state.current_sequence_size resulting: {} desired: {}", resulting_state.current_sequence_size, desired_state.current_sequence_size);
+        assert_eq!(resulting_state.next_player_try, desired_state.next_player_try,"testing state.next_player_try resulting: {} desired: {}", resulting_state.next_player_try, desired_state.next_player_try);
+    }
+
+    #[test]
+    fn test_color_pressed_event_round_success() {
+        let mut seq = [0; 10];
+        seq[0] = b'G';
+
+        let mut initial_state = State {
+          status: Status::AwaitingInput,
+          sequence: seq,
+          current_sequence_size: 1,
+          next_player_try: 0,
+        };
+
+        let desired_state = State {
+          status: Status::RoundSuccess,
+          sequence: seq,
+          current_sequence_size: 1,
+          next_player_try: 1,
+        };
+
+        let resulting_state = state_machine(&mut initial_state, Event::ColorPressed, b'G');
+        assert_eq!(resulting_state.status, desired_state.status, "testing state.status resulting: {:#?} desired: {:#?}", resulting_state.status, desired_state.status);
+        assert_eq!(resulting_state.sequence, desired_state.sequence,"testing state.sequence resulting: {:?} desired: {:?}", resulting_state.sequence, desired_state.sequence);
+        assert_eq!(resulting_state.current_sequence_size, desired_state.current_sequence_size,"testing state.current_sequence_size resulting: {} desired: {}", resulting_state.current_sequence_size, desired_state.current_sequence_size);
+        assert_eq!(resulting_state.next_player_try, desired_state.next_player_try,"testing state.next_player_try resulting: {} desired: {}", resulting_state.next_player_try, desired_state.next_player_try);
+    }
+
+    #[test]
+    fn test_color_pressed_event_game_success() {
+        let mut seq = [0; 10];
+        seq[9] = b'G';//Last in sequence
+
+        let mut initial_state = State {
+          status: Status::AwaitingInput,
+          sequence: seq,
+          current_sequence_size: 10,
+          next_player_try: 9,
+        };
+
+        let desired_state = State {
+          status: Status::GameCompleted,
+          sequence: seq,
+          current_sequence_size: 10,
+          next_player_try: 9,
+        };
+
+        let resulting_state = state_machine(&mut initial_state, Event::ColorPressed, b'G');
+        assert_eq!(resulting_state.status, desired_state.status, "testing state.status resulting: {:#?} desired: {:#?}", resulting_state.status, desired_state.status);
+        assert_eq!(resulting_state.sequence, desired_state.sequence,"testing state.sequence resulting: {:?} desired: {:?}", resulting_state.sequence, desired_state.sequence);
+        assert_eq!(resulting_state.current_sequence_size, desired_state.current_sequence_size,"testing state.current_sequence_size resulting: {} desired: {}", resulting_state.current_sequence_size, desired_state.current_sequence_size);
+        assert_eq!(resulting_state.next_player_try, desired_state.next_player_try,"testing state.next_player_try resulting: {} desired: {}", resulting_state.next_player_try, desired_state.next_player_try);
+    }
+
+    #[test]
+    fn test_color_pressed_ignore_playing_sequence() {
+        let mut seq = [0; 10];
+        seq[0] = b'G';//Last in sequence
+
+        let mut desired_state = State {
+          status: Status::PlayingSequence,
+          sequence: seq,
+          current_sequence_size: 1,
+          next_player_try: 0,
+        };
+
+        let resulting_state = state_machine(&mut desired_state, Event::ColorPressed, b'G');
+        assert_eq!(resulting_state.status, desired_state.status, "testing state.status resulting: {:#?} desired: {:#?}", resulting_state.status, desired_state.status);
+        assert_eq!(resulting_state.sequence, desired_state.sequence,"testing state.sequence resulting: {:?} desired: {:?}", resulting_state.sequence, desired_state.sequence);
+        assert_eq!(resulting_state.current_sequence_size, desired_state.current_sequence_size,"testing state.current_sequence_size resulting: {} desired: {}", resulting_state.current_sequence_size, desired_state.current_sequence_size);
+        assert_eq!(resulting_state.next_player_try, desired_state.next_player_try,"testing state.next_player_try resulting: {} desired: {}", resulting_state.next_player_try, desired_state.next_player_try);
+    }
+
+    #[test]
+    fn test_reset_requested_from_playing_sequence() {
+      let mut seq = [0; 10];
+        seq[0] = b'G';
+
+        let mut initial_state = State {
+          status: Status::PlayingSequence,
+          sequence: seq,
+          current_sequence_size: 1,
+          next_player_try: 0,
+        };
+
+        let desired_state = State {
+          status: Status::Startup,
+          sequence: [0; 10],
+          current_sequence_size: 0,
+          next_player_try: 0,
+        };
+
+        let resulting_state = state_machine(&mut initial_state, Event::ResetRequested, b'G');
+        assert_eq!(resulting_state.status, desired_state.status, "testing state.status resulting: {:#?} desired: {:#?}", resulting_state.status, desired_state.status);
+        assert_eq!(resulting_state.sequence, desired_state.sequence,"testing state.sequence resulting: {:?} desired: {:?}", resulting_state.sequence, desired_state.sequence);
+        assert_eq!(resulting_state.current_sequence_size, desired_state.current_sequence_size,"testing state.current_sequence_size resulting: {} desired: {}", resulting_state.current_sequence_size, desired_state.current_sequence_size);
+        assert_eq!(resulting_state.next_player_try, desired_state.next_player_try,"testing state.next_player_try resulting: {} desired: {}", resulting_state.next_player_try, desired_state.next_player_try);
     }
 }
