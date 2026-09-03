b37161705fbcc186ad5db64432392af4f75a9c80
Cesar de Barros <cesar@insiderstore.com.br>
2026-09-03 14:31:54 -0300

    fix: fmt

--- src/bin/blink_with_xbox/init.rs
+++ src/bin/blink_with_xbox/init.rs
@@ -1,52 +1,51 @@
 use esp_hal::gpio::{Level, Output, OutputConfig};
 
-use esp_hal::usb_serial_jtag::UsbSerialJtag;
 use esp_hal::peripherals::Peripherals;
+use esp_hal::usb_serial_jtag::UsbSerialJtag;
 
-use rc_car::{wait, blink_once};
+use rc_car::{blink_once, wait};
 
 pub fn init(peripherals: Peripherals) -> ! {
-  let mut usb_serial = UsbSerialJtag::new(peripherals.USB_DEVICE);
+    let mut usb_serial = UsbSerialJtag::new(peripherals.USB_DEVICE);
 
-  let output_config = OutputConfig::default();
-  let mut blue_led = Output::new(peripherals.GPIO5, Level::Low, output_config);
-  let mut green_led = Output::new(peripherals.GPIO6, Level::Low, output_config);
-  let mut red_led = Output::new(peripherals.GPIO7, Level::Low, output_config);
-  let mut yellow_led = Output::new(peripherals.GPIO15, Level::Low, output_config);
-  
-  let mut incorrect_led = Output::new(peripherals.GPIO16, Level::Low, output_config);
-  let mut correct_led = Output::new(peripherals.GPIO17, Level::Low, output_config);
+    let output_config = OutputConfig::default();
+    let mut blue_led = Output::new(peripherals.GPIO5, Level::Low, output_config);
+    let mut green_led = Output::new(peripherals.GPIO6, Level::Low, output_config);
+    let mut red_led = Output::new(peripherals.GPIO7, Level::Low, output_config);
+    let mut yellow_led = Output::new(peripherals.GPIO15, Level::Low, output_config);
 
-  // boot signature
-  blink_once(&mut green_led);
-  blink_once(&mut red_led);
-  blink_once(&mut blue_led);
-  blink_once(&mut yellow_led);
+    let mut incorrect_led = Output::new(peripherals.GPIO16, Level::Low, output_config);
+    let mut correct_led = Output::new(peripherals.GPIO17, Level::Low, output_config);
 
-  blink_once(&mut incorrect_led);
-  blink_once(&mut correct_led);
-  wait(500);
-  blink_once(&mut incorrect_led);
-  blink_once(&mut correct_led);
+    // boot signature
+    blink_once(&mut green_led);
+    blink_once(&mut red_led);
+    blink_once(&mut blue_led);
+    blink_once(&mut yellow_led);
 
+    blink_once(&mut incorrect_led);
+    blink_once(&mut correct_led);
+    wait(500);
+    blink_once(&mut incorrect_led);
+    blink_once(&mut correct_led);
 
-  loop {
-    if let Ok(command) = usb_serial.read_byte() {
-        match command {
-            b'G' => {
-                blink_once(&mut green_led);
+    loop {
+        if let Ok(command) = usb_serial.read_byte() {
+            match command {
+                b'G' => {
+                    blink_once(&mut green_led);
+                }
+                b'R' => {
+                    blink_once(&mut red_led);
+                }
+                b'B' => {
+                    blink_once(&mut blue_led);
+                }
+                b'Y' => {
+                    blink_once(&mut yellow_led);
+                }
+                _ => {}
             }
-            b'R' => {
-                blink_once(&mut red_led);
-            }
-            b'B' => {
-                blink_once(&mut blue_led);
-            }
-            b'Y' => {
-                blink_once(&mut yellow_led);
-            }
-            _ => {}
         }
     }
-  }
 }
