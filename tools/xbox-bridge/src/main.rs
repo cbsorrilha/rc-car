d564cdca87e0eed491f2aba5f944d13fccff7997
Cesar de Barros <cesar@insiderstore.com.br>
2026-09-03 14:30:22 -0300

    feat: adiciona start e le logs da porta serial no xbox bridge

--- tools/xbox-bridge/src/main.rs
+++ tools/xbox-bridge/src/main.rs
@@ -1,5 +1,8 @@
 use gilrs::{Button, Event, EventType, GamepadId, Gilrs};
-use std::{io::Write, time::Duration};
+use std::{
+  io::{self, Read, Write}, 
+  time::Duration
+};
 
 fn no_op(_a: Option<GamepadId>) {}
 
@@ -14,6 +17,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut active_gamepad = None;
 
+    //TODO pensar num jeito de achar o path automaticamente.
     let mut open_port = serialport::new("/dev/cu.usbmodem2101", 115200)
         .timeout(Duration::from_millis(100))
         .open()?;
@@ -21,11 +25,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     loop {
         // Examine new events
         while let Some(Event { id, event, .. }) = gilrs.next_event() {
+          // println!("Enviei: {:?}", event);
             let command = match event {
                 EventType::ButtonPressed(Button::South, _) => b'G', // A → verde
                 EventType::ButtonPressed(Button::East, _) => b'R',  // B → vermelho
                 EventType::ButtonPressed(Button::West, _) => b'B',  // X → azul
                 EventType::ButtonPressed(Button::North, _) => b'Y', // Y → Amarelo
+                EventType::ButtonPressed(Button::Start, _) => b'S', // S → Start
                 _ => continue,
             };
 
@@ -34,6 +40,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
             println!("Enviei: {}", command as char);
             active_gamepad = Some(id);
+        }
+
+        let mut buffer = [0_u8; 256];
+
+        match open_port.read(&mut buffer) {
+            Ok(size) => {
+                print!("{}", String::from_utf8_lossy(&buffer[..size]));
+            }
+            Err(error) if error.kind() == io::ErrorKind::TimedOut => {}
+            Err(error) => return Err(error.into()),
         }
 
         no_op(active_gamepad)
